4262f7a755018563783dced215d2b7e8d073b629
ombhombe2344 <ombhombe15@gmail.com>
2024-10-08 13:06:24 +0530

    Create single.cpp

---
+++ single.cpp
@@ -0,0 +1,34 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Inheritance to perform Single Inheritance
+
+#include <iostream>
+using namespace std;
+
+class Uni {
+public:
+    string uni = "Symbiosis";
+    void discipline() {
+        cout << "Engineering" << endl;
+    }
+};
+
+class Dep : public Uni {
+public:
+    string dept = "Electronics & Communication";
+};
+
+int main() {    
+    Dep department;
+    cout << department.uni << endl;
+    department.discipline();
+    cout << department.dept << endl;
+
+    return 0;
+}
+
+
+/*Output:
+Symbiosis
+Engineering
+Electronics & Communication*/
